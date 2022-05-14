2af31101602742e352f3d3f69cec600599045408
Elayad <Elayad67@gmail.com>
2022-05-14 17:45:24 +0200

    Fonction GetTransform
    Récupère le transform du cube

---
+++ Tower Attack/Assets/dfvsfdsq.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dfvsfdsq : MonoBehaviour
+{
+    Transform transform;
+
+    
+    public void GetTransform()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+        
+}
