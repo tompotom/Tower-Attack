c196649ceb1d9c660bc812af9eede96d7341bd09
Elayad <Elayad67@gmail.com>
2022-05-17 18:54:23 +0200

    Correction de Bug
    Unity :
    
    - Ajout de Layer (Background, Below, Above)
    
    Script :
    
    - Correction du LineRenderer qui ne s'affichait pas si on ne cliquait pas sur le joueur.
    - LineRenderer sera connecté automatiquement à la troupe en question.
    
    - Enlevez l'ajout de waypoint si on reste appuyez au même endroit (1 seul avec la même coordonés sera ajoutés à la scène)

--- Tower Attack/Assets/Script/AIPathFindingCustom.cs
+++ Tower Attack/Assets/Script/AIPathFindingCustom.cs
@@ -8,6 +8,7 @@ public class AIPathFindingCustom : MonoBehaviour
 
     [Header("Components")]
     [SerializeField] private Transform target;
+    [SerializeField] private Transform waypointParent;
     [SerializeField] private LineRenderer lineRenderer;
 
     [Header("Customisable Values")]
@@ -23,6 +24,7 @@ public class AIPathFindingCustom : MonoBehaviour
     Path path;
     Seeker seeker;
     Rigidbody2D rb;
+    Vector3 temporaryWaypointValues;
     
     // Values
     int currentWaypoint = 0;
@@ -39,24 +41,37 @@ public class AIPathFindingCustom : MonoBehaviour
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
 
-        lineRenderer.enabled = false;
-        wayIndex = 1;
         move = false;
         touchStartedOnPlayer = false;
+
+
+        LineRendererSettings();
 
         // Check where is the target and draw a path.
         //seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
 
+    void LineRendererSettings()
+    {
+        wayIndex = 1;
+        
+        lineRenderer.enabled = false;
+        lineRenderer.sortingOrder = 1;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.material.color = Color.white;
+    }
+
     private void Update()
     {
-        DrawPath();
-
         if (Input.GetKeyDown(KeyCode.Return))
         {
             pressStart = true;
         }
-        
+    }
+
+    private void FixedUpdate()
+    {
+        DrawPath();   
     }
 
     void OnPathComplete(Path p)
@@ -69,28 +84,27 @@ public class AIPathFindingCustom : MonoBehaviour
         }
     }
 
-    public void OnMouseDown()
-    {
-        lineRenderer.enabled = true;
-        touchStartedOnPlayer = true;
-        lineRenderer.positionCount = 1;
-        lineRenderer.SetPosition(0, transform.position);
-    }
-
     void DrawPath()
     {
         timer += Time.deltaTime;
 
         if (Input.GetMouseButton(0) && timer > timeForNextRay)
         {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.enabled = true;
             Vector3 worldPoint = Input.mousePosition;
             worldPoint.z = Mathf.Abs(Camera.main.transform.position.z);
             Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(worldPoint);
             mouseWorldPosition.z = 0f;
 
-            //RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, mouseWorldPosition, 100f);
+            if (mouseWorldPosition == temporaryWaypointValues)
+            {
+                return;
+            }
 
-            GameObject newWaypoint = Instantiate(wayPoint, mouseWorldPosition, Quaternion.identity);
+            temporaryWaypointValues = mouseWorldPosition;
+
+            GameObject newWaypoint = Instantiate(wayPoint, mouseWorldPosition, Quaternion.identity, waypointParent);
 
             wayPoints.Add(newWaypoint);
 
@@ -111,9 +125,6 @@ public class AIPathFindingCustom : MonoBehaviour
             var newPosition = Vector3.MoveTowards(transform.position, currentPos, speedOnDeltaTime);
 
             rb.MovePosition(newPosition);
-            
-
-            //transform.Translate(newPos * speedOnDeltaTime);
 
             if (transform.position == newPosition)
             {
@@ -132,20 +143,25 @@ public class AIPathFindingCustom : MonoBehaviour
         move = false;
 
         foreach (var item in wayPoints) Destroy(item);
-
         wayPoints.Clear();
-
         wayIndex = 1;
         currentWaypoint = 0;
 
+        ClearLineRenderer();
         pressStart = false;
+    }
+
+    void ClearLineRenderer()
+    {
+        lineRenderer.positionCount = 1;
+        lineRenderer.enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Collision"))
+        if (collision.gameObject.tag == "Target")
         {
-            Debug.Log("touché");
+            Debug.Log("WIN !");
         }
     }
 }
