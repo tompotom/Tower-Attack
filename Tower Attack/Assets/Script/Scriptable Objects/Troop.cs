091b067d03cdea3f8ad198a6183dd9bdb6a7b161
tom <tom.barre@outlook.fr>
2022-05-26 10:43:17 +0200

    scriptables
    troop and tower as scriptable objects

---
+++ Tower Attack/Assets/Script/Scriptable Objects/Troop.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Troop : ScriptableObject
+{
+    public new string name;
+    public int HP;
+    public int speed;
+    public Sprite sprite;
+
+}
--- Tower Attack/Assets/Script/Towers/TowerManager.cs
+++ Tower Attack/Assets/Script/Towers/TowerManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Towers : MonoBehaviour
+public class TowerManager : MonoBehaviour
 {
     Detection _detection;
     float _timeToShoot;
